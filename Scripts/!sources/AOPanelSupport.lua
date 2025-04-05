88cf16a19031c4c50d2e81a1a8cf060f52de5c8c
OLDodin <odin_sasa@mail.ru>
2025-04-05 15:31:18 +0300

    Правки под 16.1

--- Scripts/!sources/AOPanelSupport.lua
+++ Scripts/!sources/AOPanelSupport.lua
@@ -22,7 +22,7 @@ function onAOPanelRightClick( params )
 end
 
 function onAOPanelChange( params )
-	if params.unloading and string.find(params.name, "AOPanel") then
+	if params.state == ADDON_STATE_NOT_LOADED and string.find(params.name, "AOPanel") then
 		DnD.ShowWdg(getChild(mainForm, "AORBMButton"))
 	end
 end
